c1ef11c6abd3aabbe0d6330baec7af13d81cb314
ggzor <30713864+ggzor@users.noreply.github.com>
2021-06-02 16:02:34 -0500

    Updating server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,4 +1,13 @@
 import { divide } from '@monorepo/core'
+import express from 'express'
 
-console.log(divide(100, 0))
-console.log(divide(100, 20))
+const app = express()
+
+app.get('/', (_, res) => {
+  res.send(`
+${JSON.stringify(divide(100, 0))}
+${JSON.stringify(divide(100, 20))}
+`)
+})
+
+app.listen(4000)
