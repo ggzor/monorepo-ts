f23937e91bea454d5b16ad20a71ee27b4fe77948
ggzor <30713864+ggzor@users.noreply.github.com>
2021-06-02 16:02:45 -0500

    Adding CRA Application

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,10 @@
+import { divide } from '@monorepo/core'
+
+function App() {
+  console.log(divide(100, 0))
+  console.log(divide(100, 20))
+
+  return <div>Check the console for live reload of core code</div>
+}
+
+export default App
